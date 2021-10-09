56bd8bfe538dbc7daa4952105113ef4175cc2ec0
unknown <shaunak12wankhade@ggmail.com>
2021-10-09 19:58:01 +0530

    Modified welcome message

--- EmP/Program.cs
+++ EmP/Program.cs
@@ -7,6 +7,8 @@ namespace EmP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Emp");
+
         }
     }
 }
