c32b79f5a2da74931caff48de53640eac4e91bf6
unknown <shaunak12wankhade@ggmail.com>
2021-10-09 19:53:42 +0530

    Added welcome message

---
+++ EmP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
